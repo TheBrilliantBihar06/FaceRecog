7f17e5d1643e4e3971c41fe94308782dde7a863b
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-11-07 12:46:27 +0530

    Commands and dependencies

--- app.py
+++ app.py
@@ -50,3 +50,8 @@ async def compare_faces(
 
     except Exception as e:
         return JSONResponse({"error": str(e)}, status_code=500)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
